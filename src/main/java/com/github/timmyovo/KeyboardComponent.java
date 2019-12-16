7577dadb21c156f9eb0cfa2502f942fc036d19d6
TimmyOVO <timmyovo@qq.com>
2019-12-17 01:25:55 +0800

    fix when focus on a file editor ctrl c not work.

--- src/main/java/com/github/timmyovo/KeyboardComponent.java
+++ src/main/java/com/github/timmyovo/KeyboardComponent.java
@@ -6,6 +6,8 @@ import com.intellij.ide.IdeEventQueue;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.wm.WindowManager;
 import org.apache.commons.io.IOUtils;
@@ -16,7 +18,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class KeyboardComponent implements ApplicationComponent {
     @NotNull
@@ -37,12 +41,18 @@ public class KeyboardComponent implements ApplicationComponent {
 
         IdeEventQueue ideEventQueue = IdeEventQueue.getInstance();
         ideEventQueue.addPostEventListener(event -> {
-            //System.out.println(event);
             if (event instanceof KeyEvent) {
                 KeyEvent keyEvent = (KeyEvent) event;
                 if (keyEvent.isMetaDown() && keyEvent.getKeyChar() == 'c' && keyEvent.getID() == KeyEvent.KEY_PRESSED) {
-                    Arrays.stream(ProjectManager.getInstance().getOpenProjects())
+                    List<Project> collect = Arrays.stream(ProjectManager.getInstance().getOpenProjects())
                             .filter(project -> Objects.requireNonNull(WindowManager.getInstance().suggestParentWindow(project)).isActive())
+                            .collect(Collectors.toList());
+                    if (collect.stream().map(FileEditorManager::getInstance)
+                            .anyMatch(fileEditorManager -> Arrays.stream(fileEditorManager.getAllEditors()).anyMatch(fileEditor -> fileEditor.getPreferredFocusedComponent().isFocusOwner()))) {
+                        return false;
+                    }
+
+                    collect
                             .forEach(project -> {
                                 DataManager.getInstance().getDataContextFromFocusAsync().onSuccess(dataContext -> {
                                     String path = dataContext.getData(PlatformDataKeys.VIRTUAL_FILE).getPath();
